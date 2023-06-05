cf4052ee025f6c964888fdbbe46f91513047163c
alealejo25 <alejandrogianuzzi@gmail.com>
2023-06-05 19:45:50 -0300

    mensaje de confirmación

--- app/Http/Controllers/AdministracionController.php
+++ app/Http/Controllers/AdministracionController.php
@@ -32,7 +32,7 @@ class AdministracionController extends Controller
         //dd($request);
         
         //para reasiganar un rol hacerlo desde la tabla model_has_roles, cambiar en la columna role_id
-        $user = User::find(14);
+        $user = User::find(15);
         $user->assignRole($request->rol);
        // Role::create(['name' => $request->rol]);
         return view('/');

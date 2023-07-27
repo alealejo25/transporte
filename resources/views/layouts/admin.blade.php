9ac2de0cad820d20ca6a1d1dfb330a5d92e3406c
alealejo25 <alejandrogianuzzi@gmail.com>
2023-07-27 16:01:37 -0300

    mensaje de confirmación

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -668,7 +668,7 @@ input { padding:5px; border:1px solid #999; border-radius:4px; -moz-border-radiu
     <script>
   $(document).ready(function () {
     $('#tabla').DataTable({
-      "order":{[0,"desc"]}//para ordenar por la primera columna descendente
+      "order": [[ 0, "desc" ]]
       "language":{
         "decimal": "",
         "search": "Buscar", 

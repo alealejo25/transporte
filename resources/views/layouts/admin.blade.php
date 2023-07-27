cccd5563f68268b3f5e70e455f6efc84e405f8ac
alealejo25 <alejandrogianuzzi@gmail.com>
2023-07-27 16:06:39 -0300

    mensaje de confirmación

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -43,11 +43,11 @@
 
 <!-- datatables --> 
 <script src=
-"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+"https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js">
     </script>
 <!---------------->
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
 <!-- bootstrap -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" crossorigin="anonymous"></script>

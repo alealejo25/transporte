80bbe356554ecc7ecda7847fb93b46ea1fa9b244
alealejo25 <alejandrogianuzzi@gmail.com>
2023-08-22 16:02:50 -0300

    mensaje de confirmación

--- app/Http/Controllers/BolManantialController.php
+++ app/Http/Controllers/BolManantialController.php
@@ -551,6 +551,8 @@ public function reportehstrabajadas(Request $request)
                     $datos=BoletoLeagas::select('choferesleagaslnf.apellido','choferesleagaslnf.nombre','choferesleagaslnf.legajo','boletosleagas.fecha','boletosleagas.numero','boletosleagas.pasajestotal','boletosleagas.horastotal','boletosleagas.horassobrantes','boletosleagas.horastotalalargue','boletosleagas.alargue','boletosleagas.cortado','boletosleagas.doblenegro','boletosleagas.normal')->join('choferesleagaslnf','boletosleagas.chofer_id','=','choferesleagaslnf.id')->where('chofer_id',$request->chofer_id)->whereBetween('fecha',[$fi, $ff])->orderby('choferesleagaslnf.apellido')->get();
         }
 
+
+        dd($datos);
   $pdf=\PDF::loadView('bolmanantial.reportes.reportehstrabajadas',['datos'=>$datos,'fi'=>$fi, 'ff'=>$ff])
         ->setPaper('a4','portrait');
         return $pdf->download('reportehstrabajadas.pdf');
